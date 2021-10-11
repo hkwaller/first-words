55c9676f15b0fbb13f13ee449fc075b6f79dd730
Hannes Waller <hkwaller@gmail.com>
2021-10-11 16:12:00 +0200

    add buy button to settings page as well

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,47 +1,62 @@
 import { view } from '@risingstack/react-easy-state'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, TextInput } from 'react-native'
 import { state } from 'src/backend/data'
 import { t } from 'src/backend/lang'
 import AnimalButton from 'src/components/AnimalButton'
+import BuyModal from 'src/components/buy-modal/BuyModal'
 import Screen from 'src/components/Screen'
+import SmallButton from 'src/components/SmallButton'
 import { BodyText } from 'src/components/styled'
 import { animals, colors } from 'src/config/constants'
 import LanguageSelector from '../start/components/LanguageSelector'
 import Restore from '../start/components/Restore'
 
 function Settings() {
+  const [buyModalVisible, setBuyModalVisible] = useState(false)
   return (
-    <Screen>
-      <View style={{ marginVertical: 20 }} />
-      <LanguageSelector />
-      <View style={styles.animalWrapper}>
-        <BodyText style={{ marginTop: 40 }}>{t('settings_animal')}</BodyText>
-        <View style={styles.animalContainer}>
-          {animals.map(animal => {
-            return <AnimalButton key={animal.name} {...{ animal }} />
-          })}
+    <>
+      <Screen>
+        <View style={{ marginVertical: 20 }} />
+        <LanguageSelector />
+        <View style={styles.animalWrapper}>
+          <BodyText style={{ marginTop: 40 }}>{t('settings_animal')}</BodyText>
+          <View style={styles.animalContainer}>
+            {animals.map(animal => {
+              return <AnimalButton key={animal.name} {...{ animal }} />
+            })}
+          </View>
         </View>
-      </View>
-      <View style={[styles.animalWrapper, { paddingHorizontal: 30 }]}>
-        <BodyText style={{ marginTop: 40 }}>{t('settings_name')}</BodyText>
-        <TextInput
-          placeholder={t('settings_name_placeholder')}
-          style={styles.input}
-          placeholderTextColor="gray"
-          value={state.settings.name}
-          onChangeText={text => {
-            state.settings.name = text
-          }}
-        />
-      </View>
-      {state.settings.wordsLearnt.length > 0 ? (
-        <BodyText style={{ textAlign: 'center', marginTop: 24 }}>
-          {t('words_learnt_1')} {state.settings.wordsPlayed} {t('words_learnt_2')}
-        </BodyText>
-      ) : null}
-      {!state.settings.hasPurchased ? <Restore /> : null}
-    </Screen>
+        <View style={[styles.animalWrapper, { paddingHorizontal: 30 }]}>
+          <BodyText style={{ marginTop: 40 }}>{t('settings_name')}</BodyText>
+          <TextInput
+            placeholder={t('settings_name_placeholder')}
+            style={styles.input}
+            placeholderTextColor="gray"
+            value={state.settings.name}
+            onChangeText={text => {
+              state.settings.name = text
+            }}
+          />
+        </View>
+        {state.settings.wordsLearnt.length > 0 ? (
+          <BodyText style={{ textAlign: 'center', marginTop: 24 }}>
+            {t('words_learnt_1')} {state.settings.wordsPlayed} {t('words_learnt_2')}
+          </BodyText>
+        ) : null}
+        {!state.settings.hasPurchased ? (
+          <View>
+            <SmallButton
+              title={t('buy')}
+              backgroundColor={colors.pink}
+              onPress={() => setBuyModalVisible(true)}
+            />
+            <Restore />
+          </View>
+        ) : null}
+      </Screen>
+      <BuyModal isVisible={buyModalVisible} setModalVisible={setBuyModalVisible} />
+    </>
   )
 }
 
